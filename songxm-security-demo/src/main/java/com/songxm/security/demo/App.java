a20600c42d0819db579095ab1865f1759bbaef75
xuanmings <18720987043@139.com>
2017-10-29 22:22:50 +0800

    init

---
+++ songxm-security-demo/src/main/java/com/songxm/security/demo/App.java
@@ -0,0 +1,24 @@
+package com.songxm.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@RestController
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello security framework!";
+    }
+}
